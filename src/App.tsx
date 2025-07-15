cf18c236d1074d70644d25851364704780f1cb09
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-07-15 15:09:24 +0200

    Added hello world fetch from repository and hello world create

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,72 @@ import type { HelloWorldResponse } from './utils/hello_world_response';
 
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
+  const [backendMessage, setBackendMessage] = useState('');
+  const [backendError, setBackendError] = useState('');
+  
+  const [repositoryMessage, setRepositoryMessage] = useState('');
+  const [repositoryError, setRepositoryError] = useState('');
 
+  const [newMessage, setNewMessage] = useState('');
+  const [submitStatus, setSubmitStatus] = useState('');
+  
   useEffect(() => {
     HelloWorldRepository.fetchHelloWorld().then((result) => {
       if (result.success) {
-        setMessage(result.data.message);
+        setBackendMessage(result.data.message);
       } else {
-        setError(result.error);
+        setBackendError(result.error);
+      }
+    });
+
+    HelloWorldRepository.fetchHelloWorldRepository().then((result) => {
+      if(result.success) {
+        setRepositoryMessage(result.data.message);
+      } else {
+        setRepositoryError(result.error);
       }
     });
   }, []);
+
+  const handleSubmit = async () => {
+    const result = await HelloWorldRepository.createHelloWorld(newMessage);
+    if (result.success) {
+      setSubmitStatus(`✅ Message created: ${result.data.message}`);
+    } else {
+      setSubmitStatus(`❌ Error: ${result.error}`);
+    }
+  };
 
   return (
     <>
       <div>
         <h1>Hello World from Backend:</h1>
-        {error ? (
-          <p style={{ color: 'red' }}>{error}</p>
+        {backendError ? (
+          <p style={{ color: 'red' }}>{backendError}</p>
         ) : (
-          <p>{message}</p>
+          <p>{backendMessage}</p>
         )}
+      </div>
+      <div>
+        <h1>Hello World from Repository:</h1>
+        {repositoryError ? (
+          <p style={{ color: 'red' }}>{repositoryError}</p>
+        ) : (
+          <p>{repositoryMessage}</p>
+        )}
+      </div>
+      <div>
+        <h1>Send message to Backend</h1>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type your message"
+          style={{ marginRight: '1rem' }}
+        />
+        <button onClick={handleSubmit}>Send</button>
+
+        <p>{submitStatus}</p>
       </div>
     </>
   );
--- src/repositories/hello_world_repository.ts
+++ src/repositories/hello_world_repository.ts
@@ -1,7 +1,15 @@
-import { fetchHelloWorld } from '../api/hello_world_api';
+import { createHelloWorld, fetchHelloWorld, fetchHelloWorldRepository } from '../api/hello_world_api';
 
 export const HelloWorldRepository = {
   async fetchHelloWorld() {
     return await fetchHelloWorld();
+  },
+
+  async fetchHelloWorldRepository() {
+    return await fetchHelloWorldRepository();
+  },
+
+  async createHelloWorld(message: string) {
+    return await createHelloWorld(message);
   }
 };
